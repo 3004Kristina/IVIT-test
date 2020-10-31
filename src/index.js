034c7396a92b74eb41cb63594d8195e93ada3ec9
Gerasimova Kristina <kristina.usova@gmail.com>
2020-10-31 15:24:29 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-const withDropDown = document.querySelector('.with_dropdown');
-const dropDown = document.querySelector('.dropdown');
 const mobile_menu_btn = document.querySelector('.mobile_menu_btn');
 const body = document.querySelector('body');
 const mobile_menu_bg = document.querySelector('.mobile_menu_bg');
-const dropDownToggle = document.querySelectorAll('.dropdown_toggle');
+
 
 for (let dropDownToggle of document.querySelectorAll('.with_dropdown .dropdown_toggle')) {
-    let dropDown = dropDownToggle.closest('.with_dropdown');
-    dropDown.addEventListener('click', (e) => {
+    let withDropdown = dropDownToggle.closest('.with_dropdown');
+
+    dropDownToggle.addEventListener('click', (e) => {
+        console.log('click on dropdown_toggle', dropDownToggle);
+
         e.preventDefault();
-        dropDown.classList.add('open');
+       // e.stopPropagation();
+
+        withDropdown.classList.toggle('open');
     });
 }
 
-// withDropDown.addEventListener('click', () => {
-//     if(dropDown) {
-//         dropDown.classList.add('open');
-//     }
-// });
+
+document.addEventListener('click', (e) => {
+    console.log('click on document');
+
+    for (let dropDown of document.querySelectorAll('.with_dropdown.open')) {
+        if (dropDown.contains(e.target) === false) {
+            dropDown.closest('.with_dropdown').classList.remove('open');
+        }
+    }
+});
 
 mobile_menu_btn.addEventListener('click', (e) => {
     e.preventDefault();
